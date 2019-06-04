1019f05e34ead25b9c5980bf9b2dfbdcfb8d668b
abigonion <994776610@qq.com>
2019-06-04 16:38:22 +0800

    test

--- pages/_app.js
+++ pages/_app.js
@@ -42,7 +42,6 @@ export default class MyApp extends App {
 
 
     render() {
-
         const { Component, pageProps } = this.props
         return <Container>
             <Component {...pageProps} />
